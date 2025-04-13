64b90228ad2bfecc9b49522a896d86e40c1b70a4
mur <mur@qq.com>
2025-04-13 20:37:37 +0800

    22

--- plugins/error_handler.ts
+++ plugins/error_handler.ts
@@ -7,5 +7,9 @@ export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.hook('vue:error', (error, instance, info) => {
       // handle error, e.g. report to a service
     })
+
+    nuxtApp.hook('app:error', (err ) => {
+      // handle error, e.g. report to a service
+    })
   })
   
