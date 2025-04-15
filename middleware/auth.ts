ef17e17151a8a5b6ae9eb090c36b584017b6f73d
mur <mur@qq.com>
2025-04-15 10:06:24 +0800

    333

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,6 @@
-export default defineNuxtRouteMiddleware((_to, _from) => {
-    const access = useAccessToken()
-    if (!access.value) {
+export default defineNuxtRouteMiddleware(async (_to, _from) => {
+    const access = await safeGetAccessToken()
+    if (!access || access.length === 0) {
         return navigateTo(import.meta.env.VITE_LOGIN_PAGE)
     }
 })
