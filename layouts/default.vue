e659729fabad89958a8accf861f9dd8a3aa75dcb
leejne <leejne@qq.com>
2025-05-22 10:21:00 +0800

    dev server https support

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,51 +3,51 @@ const log = logger.tag("LAYOUT");
 const route = useRoute();
 const authStore = useAuthStore();
 watchEffect(() => {
-  log.debug("routing...path is : ", route.fullPath);
-  log.debug("authStore.user is : ", authStore.user);
+    log.debug("routing...path is : ", route.fullPath);
+    log.debug("authStore.user is : ", authStore.user);
 });
 </script>
 
 <template>
-  <header class="default-layout__header flex flex-align-center gap-l">
-    <NuxtLink to="/">
-      <span>LOGO</span>
-    </NuxtLink>
-    <span class="spacer" />
-    <span>Header1</span>
-    <span>{{ authStore.user?.name ?? "" }}</span>
-    <button type="button" @click="authStore.logout(true)">Logout</button>
-  </header>
-  <main class="default-layout__body">
-    <slot />
-  </main>
-  <footer class="default-layout__footer flex-center">
-    Copyright &copy; 2023
-  </footer>
+    <header class="default-layout__header flex flex-align-center gap-l">
+        <NuxtLink to="/">
+            <span>LOGO</span>
+        </NuxtLink>
+        <span class="spacer" />
+        <span>Header1</span>
+        <span>{{ authStore.user?.name ?? "" }}</span>
+        <button type="button" @click="authStore.logout(true)">Logout</button>
+    </header>
+    <main class="default-layout__body">
+        <slot />
+    </main>
+    <footer class="default-layout__footer flex-center">
+        Copyright &copy; 2023
+    </footer>
 </template>
 
 <style lang="scss" scoped>
 .default-layout__header {
-  backdrop-filter: blur(8px);
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: var(--header-height);
-  z-index: 10;
-  background-color: var(--color-header-background);
-  box-shadow: 0 0 1px rgba(0, 0, 0, 0.25);
-  padding: 0 16px;
-  gap: 32px;
+    backdrop-filter: blur(8px);
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: var(--header-height);
+    z-index: 10;
+    background-color: var(--color-header-background);
+    box-shadow: 0 0 1px rgba(0, 0, 0, 0.25);
+    padding: 0 16px;
+    gap: 32px;
 }
 
 .default-layout__body {
-  margin: 0;
-  padding-top: var(--header-height);
-  min-height: calc(100vh - var(--footer-height));
+    margin: 0;
+    padding-top: var(--header-height);
+    min-height: calc(100vh - var(--footer-height));
 }
 
 .default-layout__footer {
-  height: var(--footer-height);
+    height: var(--footer-height);
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,72 +1,81 @@
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/test-utils", "@nuxt/eslint", "@pinia/nuxt"],
-  devtools: { enabled: true },
-  app: {
-    rootAttrs: {
-      id: "app",
+    modules: ["@nuxt/test-utils", "@nuxt/eslint", "@pinia/nuxt"],
+    devtools: { enabled: true },
+    app: {
+        rootAttrs: {
+            id: "app",
+        },
+        rootTag: "body",
+        // layoutTransition: { name: 'layout', mode: 'out-in' },
+        // pageTransition: { name: 'page', mode: 'out-in' },
+        head: {
+            title: "Nuxt3 测试",
+            meta: [
+                { name: "description", content: "Nuxt3 测试" },
+                { name: "viewport", content: "width=device-width, initial-scale=1" },
+                // // for production
+
+                // { 'http-equiv': 'content-security-policy', content: "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:" },
+                { name: "format-detection", content: "telephone=no" },
+                { name: "mobile-web-app-capable", content: "yes" },
+                { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+                { name: "apple-mobile-web-app-title", content: "Nuxt3 测试" },
+                { name: "msapplication-TileColor", content: "#da532c" },
+                { name: "msapplication-TileImage", content: "/ms-icon-144x144.png" },
+                { name: "theme-color", content: "#ffffff" },
+                { name: "og:title", content: "Nuxt3 测试" },
+                { name: "og:description", content: "Nuxt3 测试" },
+                { name: "og:image", content: "/logo.png" },
+                { name: "og:site_name", content: "Nuxt3 测试" },
+                { name: "og:type", content: "website" },
+                { name: "og:locale", content: "zh_CN" },
+            ],
+            link: [
+                { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+                // 字体懒加载
+                // {
+                //   rel: 'preload',
+                //   as: 'font',
+                //   href: '/fonts/iconfont.woff2',
+                //   crossorigin: 'anonymous',
+                // },
+                // 预获取资源
+                // {
+                //   rel: 'prefetch',
+                //   as: 'image',
+                //   href: '/logo.png',
+                // },
+            ],
+        },
     },
-    rootTag: "body",
-    // layoutTransition: { name: 'layout', mode: 'out-in' },
-    // pageTransition: { name: 'page', mode: 'out-in' },
-    head: {
-      title: "Nuxt3 测试",
-      meta: [
-        { name: "description", content: "Nuxt3 测试" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        // // for production
-        // eslint-disable-next-line vue/max-len
-        // { 'http-equiv': 'content-security-policy', content: "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:" },
-        { name: "format-detection", content: "telephone=no" },
-        { name: "mobile-web-app-capable", content: "yes" },
-        { name: "apple-mobile-web-app-status-bar-style", content: "black" },
-        { name: "apple-mobile-web-app-title", content: "Nuxt3 测试" },
-        { name: "msapplication-TileColor", content: "#da532c" },
-        { name: "msapplication-TileImage", content: "/ms-icon-144x144.png" },
-        { name: "theme-color", content: "#ffffff" },
-        { name: "og:title", content: "Nuxt3 测试" },
-        { name: "og:description", content: "Nuxt3 测试" },
-        { name: "og:image", content: "/logo.png" },
-        { name: "og:site_name", content: "Nuxt3 测试" },
-        { name: "og:type", content: "website" },
-        { name: "og:locale", content: "zh_CN" },
-      ],
-      link: [
-        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-        // 字体懒加载
-        // {
-        //   rel: 'preload',
-        //   as: 'font',
-        //   href: '/fonts/iconfont.woff2',
-        //   crossorigin: 'anonymous',
-        // },
-        // 预获取资源
-        // {
-        //   rel: 'prefetch',
-        //   as: 'image',
-        //   href: '/logo.png',
-        // },
-      ],
+    css: [
+        "@/assets/css/reset.css",
+        "@/assets/css/font.css",
+        "@/assets/css/main.css",
+    ],
+    compatibilityDate: "2025-05-15",
+    nitro: {
+        // // websocket 的代理不起作用
+        // devProxy: {
+        //   '/hub': {
+        //     target: 'ws://localhost:8001',
+        //     ws: true,
+        //     secure: false,
+        //     changeOrigin: true,
+        //   }
+        // }
     },
-  },
-  css: [
-    "@/assets/css/reset.css",
-    "@/assets/css/font.css",
-    "@/assets/css/main.css",
-  ],
-  compatibilityDate: "2025-05-15",
-  nitro: {
-    // // websocket 的代理不起作用
-    // devProxy: {
-    //   '/hub': {
-    //     target: 'ws://localhost:8001',
-    //     ws: true,
-    //     secure: false,
-    //     changeOrigin: true,
-    //   }
-    // }
-  },
-  typescript: {
-    typeCheck: true,
-  },
+    devServer: {
+        port: 3000,
+        host: "localhost",
+        https: {
+            cert: 'E:\\mkcert\\vite\\localhost+1.pem',
+            key: 'E:\\mkcert\\vite\\localhost+1-key.pem',
+        },
+    },
+    typescript: {
+        typeCheck: true,
+    },
 });
